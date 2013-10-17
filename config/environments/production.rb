447ae95c1d52f8a41ca09541a6bce9f93dfa6b3d
agulek91 <aga.ltk@gmail.com>
2013-10-17 13:54:20 -0700

    assets heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Gemmaker::Application.configure do
+Ika::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
