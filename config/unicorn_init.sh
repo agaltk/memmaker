cf9f68035ddbd0a588b8064cf278d3022962183e
agulek91 <aga.ltk@gmail.com>
2013-11-26 15:36:36 +0100

    aaa

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -13,7 +13,7 @@ set -e
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/aga/memmaker
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="cd APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER=aga
 set -u
 
