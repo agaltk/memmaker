436635f237665892ef6081b50306bae53fe1cf66
agulek91 <aga.ltk@gmail.com>
2013-11-26 19:07:30 +0100

    sdadas

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -1,44 +1,35 @@
 #!/bin/sh
-### BEGIN INIT INFO
-# Provides:          unicorn
-# Required-Start:    $remote_fs $syslog
-# Required-Stop:     $remote_fs $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: Manage unicorn server
-# Description:       Start, stop, restart unicorn server for specific apllication
-## END INIT INFOR
 set -e
+# Example init script, this can be used with nginx, too,
+# since nginx and unicorn accept the same signals
 
+# Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/aga/memmaker
+APP_ROOT=/home/aga/memmaker/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=aga
+CMD="/usr/bin/unicorn -D -c $APP_ROOT/config/unicorn.rb"
+INIT_CONF=$APP_ROOT/config/init.conf
+action="$1"
 set -u
 
-OLD_PID="$PID.oldbin"
+test -f "$INIT_CONF" && . $INIT_CONF
+
+old_pid="$PID.oldbin"
+
+cd $APP_ROOT || exit 1
 
 sig () {
-  test -s "$PID" && kill -$1 'cat $PID'
+  test -s "$PID" && kill -$1 `cat $PID`
 }
 
 oldsig () {
-  test -s $OLD_PIN && kill -$1 'cat $OLD_PIN'
+  test -s $old_pid && kill -$1 `cat $old_pid`
 }
 
-run () {
-  if [ "$(id -un)" = "$AS_USER" ]; then
-    eval $1
-  else 
-    su -c "$1" - $AS_USER
-  fi
-}
-
-case "$1" in
+case $action in
 start)
-  sig 0 && echo>&2 "Alreade running" && exit 0
-  run "$CMD"
+  sig 0 && echo >&2 "Already running" && exit 0
+  $CMD
   ;;
 stop)
   sig QUIT && exit 0
@@ -51,34 +42,33 @@ force-stop)
 restart|reload)
   sig HUP && echo reloaded OK && exit 0
   echo >&2 "Couldn't reload, starting '$CMD' instead"
-  run "$CMD"
+  $CMD
   ;;
 upgrade)
   if sig USR2 && sleep 2 && sig 0 && oldsig QUIT
   then
     n=$TIMEOUT
-    while test -s $OLD_PIN && test $n -ge 0
+    while test -s $old_pid && test $n -ge 0
     do
       printf '.' && sleep 1 && n=$(( $n - 1 ))
     done
     echo
-  
-    if test $n - lt 0 && test -s $OLD_PIN
+
+    if test $n -lt 0 && test -s $old_pid
     then
-      echo >&2 "$OLD_PIN still exists after $TIMEOUT seconds"
+      echo >&2 "$old_pid still exists after $TIMEOUT seconds"
       exit 1
     fi
     exit 0
   fi
-  echo >&2 "Couldn't upgrade, starting, '$CMD' instead"
-  run "$CMD"
+  echo >&2 "Couldn't upgrade, starting '$CMD' instead"
+  $CMD
   ;;
 reopen-logs)
   sig USR1
   ;;
 *)
-  echo >&2 "Usage: $0 <start|stop|restert|upgrade|force-stop|reopen-logs>"
+  echo >&2 "Usage: $0 <start|stop|restart|upgrade|force-stop|reopen-logs>"
   exit 1
   ;;
 esac
-
