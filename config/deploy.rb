1ed6e2296135787ce872a7e24b9d9f0c531e1e4a
Agnieszka <aga.ltk@gmail.com>
2013-12-03 05:13:05 -0800

    repository changed

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:agulek91/memmaker.git"
+set :repository, "origin git@gitlab.com:Lutak/memmaker.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
