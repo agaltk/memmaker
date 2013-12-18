b185db36b15efdcacffb13a6dc5d09a44ab5d2b6
Agnieszka <aga.ltk@gmail.com>
2013-12-18 09:44:01 -0800

    new

--- app/models/picturemem.rb
+++ app/models/picturemem.rb
@@ -5,10 +5,10 @@ class Picturemem < ActiveRecord::Base
 
   def self.show_mems(type,current_user)
     if type == "my"
-      @mems = where(user_id: current_user.id)
+      @mems = where(user_id: current_user.id).order(created_at: :desc)
       
 	  else
-	    @mems = Picturemem.all	
+	    @mems = Picturemem.all.order(created_at: :desc)
     end
     @mems
   end
