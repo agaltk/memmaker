5a89661c7d298512e873642d3ab81ec292685d60
deployer <deployer@memtest.(none)>
2014-01-06 12:40:23 +0100

    working puma?

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,7 @@
 rails_env = ENV['RAILS_ENV'] || 'development'
 
-threads 0,60
+threads 8,30
+workers 2
 
 bind "unix:///home/deployer/memmaker/shared/tmp/puma/appname-puma.sock"
 pidfile "/home/deployer/memmaker/current/tmp/puma/pid"
